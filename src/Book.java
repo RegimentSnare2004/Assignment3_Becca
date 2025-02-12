a7d46e6ab67c17c50c719e6126892ff7cc2ec02d
RegimentSnare2004 <becca.godfrey@bruins.belmont.edu>
2025-02-11 22:06:16 -0600

    Added Book class

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    public String title;
+    public String author;
+    public double price;
+    public Book(String title, String author, double price){
+        this.title = title;
+        this.author = author;
+        this.price = price; 
+    }
+}
